aad7fc9dce34ac4caa24a59526dd747589bf3b17
shaddy288 <52369429+shaddy288@users.noreply.github.com>
2022-11-22 22:12:16 +0530

    Add files via upload

---
+++ digital/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    //team owl closure//
+    $('.owl-carousel').owlCarousel({
+        // autoplay: true,
+        autoplayhoverpause: true,
+        autoplaytimeout:100,
+        items:3,
+        nav:true,
+        loop:true,
+    })
+    // //Satisfied closure//
+    // $('.Satisfied').owlCarousel({
+    //    // autoplay: true,
+    //    autoplayhoverpause: true,
+    //    autoplaytimeout:100,
+    //    items:2,
+    //    loop:true, 
+    // })
+    
+});
